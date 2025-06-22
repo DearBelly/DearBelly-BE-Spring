fb5174896ffb46944413f027d1fa85e83a02bd0f
kite_U <yurry45@naver.com>
2025-06-23 06:52:45 +0900

    fix: cors 설정 http 필터에 추가

--- src/main/java/com/hanium/mom4u/global/config/SecurityConfig.java
+++ src/main/java/com/hanium/mom4u/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.hanium.mom4u.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
