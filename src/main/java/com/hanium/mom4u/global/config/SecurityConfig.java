057a4b6d1524f04a7d13531495beed8527955d2f
kite_U <yurry45@naver.com>
2025-06-23 06:52:28 +0900

    fix: cors 설정 http 필터에 추가

--- src/main/java/com/hanium/mom4u/global/config/SecurityConfig.java
+++ src/main/java/com/hanium/mom4u/global/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
