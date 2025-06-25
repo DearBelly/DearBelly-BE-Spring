caa09ca583fe694ce8ea61f49488d6c4873b2bba
hyo-lin <lin1137001@gmail.com>
2025-06-25 21:50:51 +0900

    feat: ScheduleRepository에 월별/일별 일정 조회 메서드 추가

--- src/main/java/com/hanium/mom4u/domain/calendar/repository/ScheduleRepository.java
+++ src/main/java/com/hanium/mom4u/domain/calendar/repository/ScheduleRepository.java
@@ -1,7 +1,15 @@
 package com.hanium.mom4u.domain.calendar.repository;
 
 import com.hanium.mom4u.domain.calendar.entity.Schedule;
+import com.hanium.mom4u.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findAllByMemberAndStartDateBetween(Member member, LocalDate start, LocalDate end);
+
+    List<Schedule> findAllByMemberAndStartDate(Member member, LocalDate date);
+
 }
