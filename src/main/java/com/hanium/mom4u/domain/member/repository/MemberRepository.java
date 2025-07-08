477c536ab5f440926aa9712c38f30ac5b7c0857c
hyo-lin <lin1137001@gmail.com>
2025-07-08 12:34:08 +0900

    feat: 7일 지난 비활성 회원 조회 메서드 추가

--- src/main/java/com/hanium/mom4u/domain/member/repository/MemberRepository.java
+++ src/main/java/com/hanium/mom4u/domain/member/repository/MemberRepository.java
@@ -5,11 +5,17 @@ import com.hanium.mom4u.domain.member.common.SocialType;
 import com.hanium.mom4u.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmailAndSocialType(String email, SocialType socialType);
 
     List<Member> findByFamily(Family family);
+
+    Optional<Member> findByEmailAndSocialTypeAndIsInactiveFalse(String email, SocialType socialType);
+
+    List<Member> findAllByIsInactiveTrueAndInactiveDateBefore(LocalDate date);
+
+
 }
