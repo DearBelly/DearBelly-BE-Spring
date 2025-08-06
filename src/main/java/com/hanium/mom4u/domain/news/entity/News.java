9bdb75abc48831517552a6eb924a6f140977d14c
kite_U <yurry45@naver.com>
2025-08-06 20:53:54 +0900

    feat: News 삽입 쿼리 JDBC 템플릿 작성

--- src/main/java/com/hanium/mom4u/domain/news/entity/News.java
+++ src/main/java/com/hanium/mom4u/domain/news/entity/News.java
@@ -1,5 +1,6 @@
 package com.hanium.mom4u.domain.news.entity;
 
+import com.hanium.mom4u.domain.common.BaseEntity;
 import com.hanium.mom4u.domain.news.common.Category;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -7,17 +8,22 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "news")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class News {
+public class News extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "news_id")
     private Long id;
+
+    @Column(name = "post_id")
+    private Long postId;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "category")
@@ -37,4 +43,7 @@ public class News {
 
     @Column(name = "img_url")
     private String imgUrl;
+
+    @Column(name = "posted_at")
+    private LocalDate postedAt;
 }
