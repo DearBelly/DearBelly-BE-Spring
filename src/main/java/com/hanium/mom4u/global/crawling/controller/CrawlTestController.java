0c6c824800df89c999b02ae66e42815f8c6fbe58
kite_U <yurry45@naver.com>
2025-08-06 20:54:26 +0900

    test: News 이벤트 발행을 위한 컨트롤러 생성(임시)

--- src/main/java/com/hanium/mom4u/global/crawling/controller/CrawlTestController.java
+++ src/main/java/com/hanium/mom4u/global/crawling/controller/CrawlTestController.java
@@ -1,7 +1,11 @@
 package com.hanium.mom4u.global.crawling.controller;
 
+import com.hanium.mom4u.domain.news.service.S3JsonImportEvent;
+import com.hanium.mom4u.external.s3.FileStorageService;
 import com.hanium.mom4u.global.crawling.service.CrawlingTriggerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +18,22 @@ public class CrawlTestController {
 
     private final CrawlingTriggerService crawlingTriggerService;
 
+    private final FileStorageService fileStorageService;
+
+    private final ApplicationEventPublisher eventPublisher;
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucketName;
+
+
     @PostMapping
     public ResponseEntity<String> startCrawling() {
         crawlingTriggerService.triggerCrawling();
         return ResponseEntity.ok("크롤링 요청 완료");
+    }
+
+    @PostMapping("/s3")
+    public ResponseEntity<String> startImporting() {
+        eventPublisher.publishEvent(new S3JsonImportEvent(this, bucketName));
+        return ResponseEntity.ok("DB에 저장 완료");
     }
 }
