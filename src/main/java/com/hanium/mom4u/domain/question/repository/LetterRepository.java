af839290ba96522a11ae617de91fc0aea6564eda
hyo-lin <lin1137001@gmail.com>
2025-08-14 21:57:49 +0900

    feat: LetterRepository 추가 및 기간별 조회 쿼리 구현

---
+++ src/main/java/com/hanium/mom4u/domain/question/repository/LetterRepository.java
@@ -0,0 +1,33 @@
+package com.hanium.mom4u.domain.question.repository;
+
+import com.hanium.mom4u.domain.family.entity.Family;
+import com.hanium.mom4u.domain.member.entity.Member;
+import com.hanium.mom4u.domain.question.entity.Letter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface LetterRepository extends JpaRepository<Letter, Long> {
+
+    @Query("""
+        select i
+        from Letter i
+        where i.family = :family
+          and i.createdAt between :start and :end
+        order by i.createdAt desc
+    """)
+    List<Letter> findByFamilyAndCreatedAtBetween(
+            Family family, LocalDateTime start, LocalDateTime end);
+
+
+    @Query("""
+      select l from Letter l
+      where l.writer = :writer
+        and l.createdAt between :start and :end
+      order by l.createdAt desc
+    """)
+    List<Letter> findByWriterAndCreatedAtBetween(
+            Member writer, LocalDateTime start, LocalDateTime end);
+}
