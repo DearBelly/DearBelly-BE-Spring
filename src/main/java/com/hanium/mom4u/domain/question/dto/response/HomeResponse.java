3dc98ba12be2d69db8f62ad4886890e213aa88fc
hyo-lin <lin1137001@gmail.com>
2025-08-14 20:29:44 +0900

    feat: 아기 이름, 주차, 편지 이모티콘 여부 dto 추가

---
+++ src/main/java/com/hanium/mom4u/domain/question/dto/response/HomeResponse.java
@@ -0,0 +1,15 @@
+package com.hanium.mom4u.domain.question.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class HomeResponse {
+    private String babyName;          // 아기 이름
+    private int week;                 // 오늘 기준 0부터
+    private boolean hasUnreadLetters; // true면 편지 아이콘 표시
+}
