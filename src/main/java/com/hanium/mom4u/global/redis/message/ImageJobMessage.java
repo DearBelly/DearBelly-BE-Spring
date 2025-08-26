5da6cc2a94f43f5c508e9a0073513716e795722f
kite_U <yurry45@naver.com>
2025-08-27 04:33:25 +0900

    feat: Redis Stream Publisher 추가(FastAPI에 전송)

---
+++ src/main/java/com/hanium/mom4u/global/redis/message/ImageJobMessage.java
@@ -0,0 +1,21 @@
+package com.hanium.mom4u.global.redis.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageJobMessage {
+
+    private String correlationId;
+    private String presignedUrl;
+    private String replyQueue;
+    private String callbackUrl;
+    private String contentType;
+    private String createdAt;
+    private long ttlSec;
+}
