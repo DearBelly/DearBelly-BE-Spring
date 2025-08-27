62c593cbec61ccf1a776060dfe3d4e57ae652d19
hyo-lin <lin1137001@gmail.com>
2025-08-27 23:34:51 +0900

    refactor: lmpDate와 birth 응답 날짜 포맷을 yyyy.MM.dd로 변경

--- src/main/java/com/hanium/mom4u/domain/member/dto/request/ProfileEditRequest.java
+++ src/main/java/com/hanium/mom4u/domain/member/dto/request/ProfileEditRequest.java
@@ -1,8 +1,8 @@
 package com.hanium.mom4u.domain.member.dto.request;
 
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 @Getter
@@ -12,7 +12,6 @@ public class ProfileEditRequest {
     private String nickname;
 
     @Schema(description = "마지막 생리 시작일")
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate lmpDate;
 
     @Schema(description = "프로필 이미지 URL")
--- src/main/java/com/hanium/mom4u/domain/member/dto/response/MemberInfoResponse.java
+++ src/main/java/com/hanium/mom4u/domain/member/dto/response/MemberInfoResponse.java
@@ -1,5 +1,6 @@
 package com.hanium.mom4u.domain.member.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hanium.mom4u.domain.news.common.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,9 +15,11 @@ public class MemberInfoResponse {
     private String email;
     private String imgUrl;
     private Boolean isPregnant;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
     private LocalDate lmpDate;
     private Boolean prePregnant;
     private String gender;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
     private LocalDate birthDate;
     private Set<Category> interests;
     private String socialType;
