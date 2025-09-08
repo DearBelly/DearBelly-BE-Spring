2549798ea2d57d291efe4dda3346124e03e029e1
hyo-lin <lin1137001@gmail.com>
2025-09-08 23:47:15 +0900

    feat: FK 명시 @JoinColumn 추가

--- src/main/java/com/hanium/mom4u/domain/news/entity/NewsBookmark.java
+++ src/main/java/com/hanium/mom4u/domain/news/entity/NewsBookmark.java
@@ -5,18 +5,23 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name="news_bookmark",
-        uniqueConstraints=@UniqueConstraint(columnNames={"member_id","news_id"}))
+        uniqueConstraints=@UniqueConstraint(columnNames={"member_id","news_id"})) // 이 부분은 아래와 같이 수정될 겁니다.
 public class NewsBookmark {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     @ManyToOne
+    @JoinColumn(name = "member_id")
     Member member;
-    @ManyToOne News news;
+
+    @ManyToOne
+    @JoinColumn(name = "news_id")
+    News news;
+
     protected NewsBookmark() {}
     public NewsBookmark(Member member, News news) {
         this.member = member;
         this.news = news;
     }
 }
-
