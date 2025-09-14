9c6c58c32a385afc125bb0e215c532baf4fd3ee3
hyo-lin <lin1137001@gmail.com>
2025-09-14 21:40:17 +0900

    feat: 가족코드에 @PreAuthorize(USER) 적용

--- src/main/java/com/hanium/mom4u/domain/family/controller/FamilyController.java
+++ src/main/java/com/hanium/mom4u/domain/family/controller/FamilyController.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +19,7 @@ import java.util.List;
 public class FamilyController {
     private final FamilyService familyService;
 
+    @PreAuthorize("hasRole('USER')")
     @Operation(summary = "가족 코드 생성 API", description = "임산부만 호출할 수 있으며, 3분 유효한 공유 코드를 생성합니다.")
     @PostMapping("/api/v1/family-code")
     public ResponseEntity<CommonResponse> generateFamilyCode() {
@@ -26,6 +27,7 @@ public class FamilyController {
         return ResponseEntity.ok(CommonResponse.onSuccess(code));
     }
 
+    @PreAuthorize("hasRole('USER')")
     @Operation(summary = "가족 참여 API", description = "가족 코드를 입력해 3분 내 공유에 참여합니다.")
     @PostMapping("/api/v1/family-code/join")
     public ResponseEntity<CommonResponse> joinFamily(@RequestBody FamilyCodeRequest request){
@@ -33,15 +35,12 @@ public class FamilyController {
         return ResponseEntity.ok(CommonResponse.onSuccess("참여 성공"));
     }
 
-
+    @PreAuthorize("hasRole('USER')")
     @Operation(summary = "가족 코드 유효성 확인 + 사용자 목록 조회", description = "가족 코드가 유효한 경우 사용자 목록 반환. 유효하지 않으면 예외 발생")
     @GetMapping("/api/v1/family-code/members")
     public ResponseEntity<CommonResponse> getFamilyMembers() {
         List<FamilyMemberResponse> members = familyService.getFamilyMembersByFamily();
         return ResponseEntity.ok(CommonResponse.onSuccess(members));
     }
-
-
-
 
 }
