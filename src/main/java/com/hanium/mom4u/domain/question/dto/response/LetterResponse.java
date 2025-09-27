cad2692ea0be4ea878b5f2fa5159ff08888b364f
youyeon11 <jeonyuyeon@jeon-yuyeon-ui-Macmini.local>
2025-09-27 12:53:13 +0900

    chore: 스키마 설명 추가

--- src/main/java/com/hanium/mom4u/domain/question/dto/response/LetterResponse.java
+++ src/main/java/com/hanium/mom4u/domain/question/dto/response/LetterResponse.java
@@ -1,6 +1,7 @@
 package com.hanium.mom4u.domain.question.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,15 +10,24 @@ import java.time.LocalDateTime;
 @Builder
 @Getter
 @AllArgsConstructor
+@Schema(description = "편지 조회 관련 응답 DTO")
 public class LetterResponse {
 
+    @Schema(name = "편지 고유 ID")
     private Long id;
+    @Schema(name = "편지 내용")
     private String content;
+    @Schema(name = "작성자 닉네임")
     private String nickname;
+    @Schema(name = "작성자 프로필 사진")
     private String imgUrl;
+
+    @Schema(name = "작성 날짜")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
     private LocalDateTime createdAt;
+    @Schema(name = "내가 쓴 글인지의 여부(본인 작성자인지)")
     private boolean editable; // 내가 쓴 글인지
+    @Schema(name = "")
     private String question;
 
 
