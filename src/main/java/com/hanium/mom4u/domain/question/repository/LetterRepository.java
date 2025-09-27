080a82ef37e7454db54afaf7a0dea0c4bd1dca90
youyeon11 <jeonyuyeon@jeon-yuyeon-ui-Macmini.local>
2025-09-27 12:52:57 +0900

    feat: 당일에 작성된 편지 중에서 읽지 않은 것 확인 쿼리 구현

--- src/main/java/com/hanium/mom4u/domain/question/repository/LetterRepository.java
+++ src/main/java/com/hanium/mom4u/domain/question/repository/LetterRepository.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public interface LetterRepository extends JpaRepository<Letter, Long> {
+public interface LetterRepository extends JpaRepository<Letter, Long>, LetterRepositoryCustom {
 
     @Query("""
         select l from Letter l
