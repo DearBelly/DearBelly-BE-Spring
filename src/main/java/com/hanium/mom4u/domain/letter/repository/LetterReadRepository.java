9b3bad1b4040e9add33df77542d519fb97920b0f
hyo-lin <lin1137001@gmail.com>
2025-10-11 16:15:21 +0900

    refactor: LetterReadRepository의 count 쿼리를 existsBy 메서드로 단순화

--- src/main/java/com/hanium/mom4u/domain/letter/repository/LetterReadRepository.java
+++ src/main/java/com/hanium/mom4u/domain/letter/repository/LetterReadRepository.java
@@ -12,26 +12,15 @@ import java.util.Optional;
 public interface LetterReadRepository extends JpaRepository<LetterRead, Long> {
 
     /** 특정 편지/회원의 읽음 엔티티 조회 */
-    Optional<LetterRead> findByLetter_IdAndMember_Id(
-            @Param("letterId") Long letterId, @Param("memberId") Long memberId);
+    Optional<LetterRead> findByLetter_IdAndMember_Id(Long letterId, Long memberId);
+
 
     /** 편지 단건에 대해 '읽었는지' 여부 (readAt not null) */
-    @Query("""
-        select (count(lr) > 0)
-        from LetterRead lr
-        where lr.letter.id = :letterId
-          and lr.member.id = :memberId
-          and lr.readAt is not null
-    """)
-    boolean findExistByLetterIdAndMemberId(
-            @Param("letterId") Long letterId, @Param("memberId") Long memberId);
+
+    boolean existsByLetter_IdAndMember_IdAndReadAtIsNotNull(Long letterId, Long memberId);
+
 
     /** 회원 기준으로 아직 읽지 않은 편지가 존재하는지 (날짜 무관, readAt is null) */
-    @Query("""
-        select (count(lr) > 0)
-        from LetterRead lr
-        where lr.member.id = :memberId
-          and lr.readAt is null
-    """)
-    boolean existsUnreadByMemberId(@Param("memberId") Long memberId);
+
+    boolean existsByMember_IdAndReadAtIsNull(Long memberId);
 }
