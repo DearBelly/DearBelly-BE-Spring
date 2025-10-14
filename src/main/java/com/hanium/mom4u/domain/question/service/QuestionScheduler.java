2a0810e38e962ddf8e955bc57fde459768103a3e
youyeon11 <yurry45@naver.com>
2025-10-14 12:56:41 +0900

    feat: 트랜잭션 분리 및 락 설정

--- src/main/java/com/hanium/mom4u/domain/question/service/QuestionScheduler.java
+++ src/main/java/com/hanium/mom4u/domain/question/service/QuestionScheduler.java
@@ -1,10 +1,13 @@
 package com.hanium.mom4u.domain.question.service;
 
+import com.hanium.mom4u.global.exception.GeneralException;
+import com.hanium.mom4u.global.response.StatusCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -15,9 +18,15 @@ public class QuestionScheduler {
 
     /** 매일 0시: 트리거(신호) */
     @Async("schedulerExecutor")
+    @Transactional
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void scheduleEnsureToday() {
-        log.info("[SCHED] ensureTodayGlobalQuestion START");
-        questionService.ensureTodayGlobalQuestionAsync(); // ★ 서비스 호출
+        try {
+            log.info("[SCHED] ensureTodayGlobalQuestion START");
+            questionService.ensureTodayGlobalQuestion();
+        } catch (Exception e) {
+            log.error("[SCHED] Daily Question Scheduling FAILED", e);
+            throw GeneralException.of(StatusCode.QUESTION_SCHEDULER_ERROR);
+        }
     }
 }
