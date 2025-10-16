4edda8cf2f2c7e7e849d19a87b80c6c7d9ae7deb
youyeon11 <yurry45@naver.com>
2025-10-16 22:17:58 +0900

    refactor: Configuration 분리

--- src/main/java/com/hanium/mom4u/Mom4uApplication.java
+++ src/main/java/com/hanium/mom4u/Mom4uApplication.java
@@ -2,14 +2,10 @@ package com.hanium.mom4u;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableAsync
 public class Mom4uApplication {
 
     public static void main(String[] args) {
